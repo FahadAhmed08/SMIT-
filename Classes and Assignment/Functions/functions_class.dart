0ba6618e51af35cd0c57a2609c7e75d5b3e2f6dc
FahadAmed08 <fahadsimari111@gmail.com>
2024-01-29 20:58:03 +0500

    function ke zarie table kaise print krenge

---
+++ Classes and Assignment/Functions/functions_class.dart
@@ -0,0 +1,12 @@
+void main() {
+  table(6);
+}
+
+table(int Number) {
+  for (var i = 1; i <= 10; i++) {
+    print("$Number x $i = ${Number * i}");
+  }
+}
+// null safety 
+// types of function
+// ternory operators
